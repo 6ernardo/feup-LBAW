5cc1631f6942cf51b9a539837f8627758ce97eb6
Nuno Martim Viana Penafort <up202008405@fe.up.pt>
2023-10-25 08:53:15 +0000

    Upload New File

---
+++ database/indices.sql
@@ -0,0 +1,35 @@
+-- Performance Indices
+CREATE INDEX author_questions ON question btree(author_id);
+CREATE INDEX notification_user ON notification btree(user_id);
+CREATE INDEX tag_question ON questions_tag btree(tag_id);
+
+-- Full-text Search Indices
+ALTER TABLE question
+ADD COLUMN tsvectors TSVECTOR;
+
+CREATE FUNCTION question_search_update() RETURNS TRIGGER AS $$
+BEGIN
+ IF TG_OP = 'INSERT' THEN
+        NEW.tsvectors = (
+         setweight(to_tsvector('english', NEW.title), 'A') ||
+         setweight(to_tsvector('english', NEW.description), 'B')
+        );
+ END IF;
+ IF TG_OP = 'UPDATE' THEN
+         IF (NEW.title <> OLD.title OR NEW.description <> OLD.description) THEN
+           NEW.tsvectors = (
+             setweight(to_tsvector('english', NEW.title), 'A') ||
+             setweight(to_tsvector('english', NEW.description), 'B')
+           );
+         END IF;
+ END IF;
+ RETURN NEW;
+END $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER question_search_update
+ BEFORE INSERT OR UPDATE ON question
+ FOR EACH ROW
+ EXECUTE PROCEDURE question_search_update();
+
+CREATE INDEX search_idx ON question USING GIN (tsvectors);
