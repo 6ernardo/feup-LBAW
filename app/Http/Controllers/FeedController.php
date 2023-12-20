af408c7f7f912ef34ef83eec23709d8321b0e284
NunoPenafort <up202008405@up.pt>
2023-12-20 20:09:24 +0000

    Added logic to display followed questions/tags

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Question;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Tag;
 
 class FeedController extends Controller{
     public function index(){
@@ -19,10 +22,31 @@ class FeedController extends Controller{
             $question->save();
         }
         */
+        if (Auth::check()) {
+            $id = Auth::id();
+            $user = User::find($id);
 
-        return view('pages.feed', [
-            'topQuestions' => $topQuestions,
-            'newQuestions' => $newQuestions
-        ]);
+            $followed_tags = $user->followed_tags()->get();
+            $followed_tag_questions = collect();
+                foreach ($followed_tags as $tag) {
+                     $followed_tag_questions = $followed_tag_questions->merge($tag->questions);
+                }
+            
+            $followed_questions = $user->followed_questions()->get();
+
+            $followed = $followed_tag_questions->merge($followed_questions);
+            
+            return view('pages.feed', [
+                'topQuestions' => $topQuestions,
+                'newQuestions' => $newQuestions,
+                'followedQuestions' => $followed
+            ]);
+        }
+        else{
+            return view('pages.feed', [
+                'topQuestions' => $topQuestions,
+                'newQuestions' => $newQuestions
+            ]);
+        }
     }
 }
