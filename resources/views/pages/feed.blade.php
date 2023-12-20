922f0f9a571452f3c5efbe8501bc878a3066d816
Bernardo <6ernardocampos@gmail.com>
2023-12-20 19:04:40 +0000

    Footer and feed

--- resources/views/pages/feed.blade.php
+++ resources/views/pages/feed.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<section id="feed">
 <a class="button" href="{{ url('/questions/create') }}"> Post Question </a>
 
 <div id="aba_buttons">
@@ -8,16 +9,15 @@
     <button onclick="openTab('newQuestions')">New</button>
 </div>
 
-<section id="feed">
+
 
     <div id="topQuestions" class="tab-content">
-        <h1>Top Questions</h1>
+        <h2>Top Questions</h2>
         @forelse ($topQuestions as $question)
             <div class="question">
                 <a href="{{ url('/questions/'.$question->question_id) }}">
                     <h2>{{ $question->title }}</h2>
-                    <p>{{ $question->description }}</p>
-                    <p>Score: {{ $question->score }}</p>
+                    <p>Score:{{ $question->score }} | posted by {{ $question->author->name }} on {{ $question->timestamp }} </p>
                 </a>
             </div>
         @empty
@@ -26,13 +26,12 @@
     </div>
 
     <div id="newQuestions" class="tab-content" style="display: none;">
-        <h1>New Questions</h1>
+        <h2>New Questions</h2>
         @forelse ($newQuestions as $question)
             <div class="question">
                 <a href="{{ url('/questions/'.$question->question_id) }}">
                     <h2>{{ $question->title }}</h2>
-                    <p>{{ $question->description }}</p>
-                    <p>Score: {{ $question->score }}</p>
+                    <p>Score:{{ $question->score }} | posted by {{ $question->author->name }} on {{ $question->timestamp }} </p>
                 </a>
             </div>
         @empty
