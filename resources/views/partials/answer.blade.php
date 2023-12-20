2a7b653d6572b776aed818ca7a264f9c99a8a5dd
Bernardo <6ernardocampos@gmail.com>
2023-12-20 21:56:46 +0000

    Question page

--- resources/views/partials/answer.blade.php
+++ resources/views/partials/answer.blade.php
@@ -9,6 +9,14 @@
         </form>
         <a class="button" href="{{ url('/answers/'.$answer->answer_id.'/edit') }}"> Edit </a>
     @endif
+
+    <div class="answer" id="answer{{ $answer->answer_id }}">
+        <button class="button-vote upvote-button" onclick="vote({{ $answer->answer_id }} , 1)">Upvote</button>
+        <span class="score">{{ $answer->score }}</span>
+        <button class="button-vote downvote-button" onclick="vote({{ $answer->answer_id }} , -1)">Downvote</button>
+        <button class="button-vote remove-vote-button" onclick="removeVote({{ $answer->answer_id }})">Remove Vote</button>
+    </div>
+
     @if (Auth::check() && $answer->question->author_id == Auth::user()->user_id)
         <form method="Post" action="{{ url('questions/'.$answer->question->question_id.'/mark_correct') }}">
             @csrf
@@ -19,13 +27,14 @@
     @endif
     <section id="answer_comments">
         <h3>Comments</h3>
-        @each('partials.commentanswer', $answer->comments, 'comment')
-    </section>
-    <section id="post_answer_comment">
+        <section id="post_answer_comment">
         <form method="POST" action="{{ url('questions/'.$answer->question_id.'/answer/'.$answer->answer_id.'/comment/create') }}">
             @csrf
             <textarea name="content" id="content" placeholder="Type your comment here..." value="{{ old('description') }}" required></textarea>
             <button type="submit">Submit</button>
         </form>
     </section>
+        @each('partials.commentanswer', $answer->comments, 'comment')
+    </section>
+    
 </article>
