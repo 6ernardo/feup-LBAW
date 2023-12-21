a526b870c6877b483c66fcd9c785dcbdaa770241
Bernardo <6ernardocampos@gmail.com>
2023-12-21 16:24:09 +0000

    Create Tag bug fix

--- resources/views/pages/tagList.blade.php
+++ resources/views/pages/tagList.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+@if(Auth::check() && Auth::user()->isAdmin())
 <a class="button" href="/tags/create">Create Tag</a>
+@endif
 <h1>All Tags</h1>
     <ul>
         @foreach($tags as $tag)
