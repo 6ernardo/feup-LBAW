3938d7b075c35853d18e26ef659050349e680002
Bernardo <6ernardocampos@gmail.com>
2023-12-21 11:11:38 +0000

    Final fix

--- app/Policies/AdminPolicy.php
+++ app/Policies/AdminPolicy.php
@@ -1,10 +1,11 @@
-    <?php
+<?php
 
 namespace App\Policies;
 
 use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminPolicy
 {
